122785a55b3860991209cb17a901282e22bc008a
Gellert <gellert.li1995@gmail.com>
2018-10-03 13:22:36 -0700

    bug fixed

--- Hash_Tables_Practice/Project1/HashTable.h
+++ Hash_Tables_Practice/Project1/HashTable.h
@@ -1,103 +1,103 @@
 #ifndef HASHTABLE_H
-#define HASHTABLE_H	
+#define HASHTABLE_H
 
 #include <iostream>
 using namespace std;
 
-const int CAP = 13;	// For testing, you can change it to another prime number.
- 
-/* define some collision methods: 
-	 0 for linear probing without c/j
-	 1 for linear probing with c/j
-	 2 for quadratic probing
-	 3 for double hashing
+const int CAP = 13;    // For testing, you can change it to another prime number.
 
-	Change the number to test different open addressing.
-*/
-constexpr unsigned int COLLISION = 2;
+/* define some collision methods:
+ 0 for linear probing without c/j
+ 1 for linear probing with c/j
+ 2 for quadratic probing
+ 3 for double hashing
+ 
+ Change the number to test different open addressing.
+ */
+constexpr unsigned int COLLISION = 0;
 
 class HashTable
 {
 public:
-
-	/*
-		Public member functions you should implement:
-			Default constructor
-				- Set all the indices of the table to -1.
-			overloaded constructor
-			the Big 5
-			accessor functions
-			mutator functions
-			isEmpty
-				- Returns a Boolean
-			insertKey 
-				- Calls the hashValue function to get the index.
-			searchKey
-				- Calls the hashValue function to get the index.
-				- Returns a Boolean value
-			any overloaded operator that could be useful
-			deleteKey 
-				- Calls the hashValue function to get the index.
-				- Mark deleted indices with -2.
-			rehash
-				- Re-create the table with double the capacity to the next prime number.
-			destructor
-			resetTable
-				- Will re-set to an empty table
-	*/
-	HashTable();
-
-	~HashTable();
-
-	HashTable(const HashTable &table);
-
-	HashTable(const int newCap);
-
-	HashTable(HashTable &&table);
-
-	int getNumberOfItems() const;
-
-	int getCapacity() const;
-
-	void setNumberOfItems(const int num);
-
-	void setCapacity(const int cap);
-
-	HashTable& operator=(const HashTable &table);
-
-	bool isEmpty() const;
-
-	void insertKey(const int key);
-
-	bool searchKey(const int key) const;
-
-	void rehash();
-
-	void deleteKey(const int key);
-
-	void resetTable();
-
-	HashTable& operator=(HashTable &&table);
-
-	friend ostream& operator<< (std::ostream& stream, const HashTable& HT);
-
+    
+    /*
+     Public member functions you should implement:
+     Default constructor
+     - Set all the indices of the table to -1.
+     overloaded constructor
+     the Big 5
+     accessor functions
+     mutator functions
+     isEmpty
+     - Returns a Boolean
+     insertKey
+     - Calls the hashValue function to get the index.
+     searchKey
+     - Calls the hashValue function to get the index.
+     - Returns a Boolean value
+     any overloaded operator that could be useful
+     deleteKey
+     - Calls the hashValue function to get the index.
+     - Mark deleted indices with -2.
+     rehash
+     - Re-create the table with double the capacity to the next prime number.
+     destructor
+     resetTable
+     - Will re-set to an empty table
+     */
+    HashTable();
+    
+    ~HashTable();
+    
+    HashTable(const HashTable &table);
+    
+    HashTable(const int newCap);
+    
+    HashTable(HashTable &&table);
+    
+    int getNumberOfItems() const;
+    
+    int getCapacity() const;
+    
+    void setNumberOfItems(const int num);
+    
+    void setCapacity(const int cap);
+    
+    HashTable& operator=(const HashTable &table);
+    
+    bool isEmpty() const;
+    
+    void insertKey(const int key);
+    
+    bool searchKey(const int key) const;
+    
+    void rehash();
+    
+    void deleteKey(const int key);
+    
+    void resetTable();
+    
+    HashTable& operator=(HashTable &&table);
+    
+    friend ostream& operator<< (std::ostream& stream, const HashTable& HT);
+    
 private:
-	/* 
-		Private member functions you should implement:
-			hashValue
-				Parameters: the key and the j value
-				Return value: an int storing an index
-			searchKey
-				Overloaded function that returns an index
-	*/
-
-	int hashValue(const int key, int j) const;
-
-	int searchKey(const float key) const;
-
-	int *ht;			//pointer to the hash table
-	int numOfItems;		//number of items in the hash table
-	int cap;			//maximum length of the hash table	
+    /*
+     Private member functions you should implement:
+     hashValue
+     Parameters: the key and the j value
+     Return value: an int storing an index
+     searchKey
+     Overloaded function that returns an index
+     */
+    
+    int hashValue(const int key, int j) const;
+    
+    int searchKey(const double key) const;
+    
+    int *ht;            //pointer to the hash table
+    int numOfItems;        //number of items in the hash table
+    int cap;            //maximum length of the hash table
 };
 
 #endif
--- Hash_Tables_Practice/Project1/Main.cpp
+++ Hash_Tables_Practice/Project1/Main.cpp
@@ -5,43 +5,72 @@ using namespace std;
 
 int main()
 {
-	// Add your testing cases here.
-
-	HashTable HT;
-
-	HT.insertKey(506643548);
-	HT.insertKey(281942902);
-	HT.insertKey(233667136);
-	HT.insertKey(155778322);
-	HT.insertKey(155778348);
-	HT.insertKey(656132722);
-	HT.insertKey(984357152);
-	HT.insertKey(687056857);
-	HT.insertKey(235463598);
-
-	cout << endl << "Hash Table: " << HT << endl << endl;
-
-	cout << "Searching 281942902..." << (HT.searchKey(281942902) ? "Found" : "Not Found") << endl;
-	cout << "Searching 155778322..." << (HT.searchKey(155778322) ? "Found" : "Not Found") << endl << endl;
-	cout << "Deleting 155778322..." << endl << endl;
-	HT.deleteKey(155778322);
-
-	cout << "Deleting 15577832..." << endl << endl;
-	HT.deleteKey(15577832);
-
-	cout << endl << "Hash Table: " << HT << endl;
-
-	HT.insertKey(155778322);
-
-	cout << endl << "Hash Table: " << HT << endl;
-
-	cout << "Reset Table" << endl << endl;
-
-	HT.resetTable();
-
-	cout << "Hash Table: " << HT << endl;
-
-	cout << endl;
-	system("Pause");
-	return 0;
+    // Add your testing cases here.
+    
+    HashTable HT;
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    
+    HT.insertKey(506643548);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    HT.insertKey(281942902);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    HT.insertKey(233667136);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    HT.insertKey(155778322);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    HT.insertKey(155778348);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    HT.insertKey(656132722);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    HT.insertKey(984357152);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    HT.insertKey(687056857);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    HT.insertKey(235463598);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    cout << endl << "Hash Table: " << HT << endl << endl;
+    
+    cout << "Searching 281942902..." << (HT.searchKey(281942902) ? "Found" : "Not Found") << endl;
+    cout << "Searching 155778322..." << (HT.searchKey(155778322) ? "Found" : "Not Found") << endl << endl;
+    cout << "Deleting 155778322..." << endl << endl;
+    HT.deleteKey(155778322);
+    
+    cout << "Deleting 15577832..." << endl << endl;
+    HT.deleteKey(15577832);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    HT.insertKey(155778322);
+    
+    cout << endl << "Hash Table: " << HT << endl;
+    
+    cout << "Reset Table" << endl << endl;
+    
+    HT.resetTable();
+    
+    cout << "Hash Table: " << HT << endl;
+    
+    cout << endl;
+    system("Pause");
+    return 0;
 }
